eb72770cadefc94b700520d8aaed0ed5478f2ad7
ireneeming <63698668+ireneeming@users.noreply.github.com>
2022-04-17 14:40:52 +0900

    toggle.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import './App.css';
-import Toggle from './Test/Toggle';
-import Tab from './Test/Tab';
-import Slider from './Test/Slider';
-import Input from './Test/Input';
-import Dropdown from './Test/Dropdown';
-import Test1 from './Test/Test1'
-import Test2 from './Test/Test2'
-
+import "./App.css";
+import Toggle from "./Test/Toggle";
+import Tab from "./Test/Tab";
+import Slider from "./Test/Slider";
+import Input from "./Test/Input";
+import Dropdown from "./Test/Dropdown";
+import Test1 from "./Test/Test1";
+import Test2 from "./Test/Test2";
 
 function App() {
   return (
     <div className="App">
-      <Toggle/>
-      <Test1/>
-      <Test2/>
-      <Tab/>
-      <Slider/>
-      <Input/>
-      <Dropdown/>
+      <Test2 />
+      <Tab />
+      <Slider />
+      <Input />
+      <Dropdown />
     </div>
   );
 }
--- src/Test/Test2.js
+++ src/Test/Test2.js
@@ -1,19 +1,50 @@
-import React,{useState} from 'react';
-import * as STC from './test2.style.js';
-
+import React, { useState } from "react";
+import * as STC from "./test2.style.js";
 
 const Test2 = () => {
+  const [select, setSelect] = useState("basic");
+  const [locate, setLocate] = useState("5px");
 
-  const [toggle, setToggle] = useState(true);
+  const handleSelectChange = (event) => {
+    const value = event.target.value;
+    console.log(value);
+    setSelect(value);
 
-  return(
-    
-      <STC.ToggleContainer>
-        <STC.ToggleBtn isSelected={toggle}>기본</STC.ToggleBtn>
-        <STC.ToggleBtn isSelected={!toggle}>상세</STC.ToggleBtn>
-      </STC.ToggleContainer>
-    
+    if (value === "basic") {
+      setLocate("5px");
+    } else {
+      setLocate("50%");
+    }
+  };
+  return (
+    <STC.ToggleContainer>
+      <STC.ToggleBtnWrap>
+        <STC.ToggleBtn>
+          <STC.ToggleRadio
+            id="radio01"
+            type="radio"
+            name="radio"
+            value="basic"
+            checked={select === "basic"}
+            onChange={(event) => handleSelectChange(event)}
+          />
+          <STC.ToggleLabel for="radio01">기본</STC.ToggleLabel>
+        </STC.ToggleBtn>
+        <STC.ToggleBtn>
+          <STC.ToggleRadio
+            id="radio02"
+            type="radio"
+            name="radio"
+            value="detail"
+            checked={select === "detail"}
+            onChange={(event) => handleSelectChange(event)}
+          />
+          <STC.ToggleLabel for="radio02">상세</STC.ToggleLabel>
+        </STC.ToggleBtn>
+      </STC.ToggleBtnWrap>
+      <STC.ToggleSlider locate={locate} />
+    </STC.ToggleContainer>
   );
-}
+};
 
 export default Test2;
